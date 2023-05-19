64e0906881e90d42369604b2d72ede1c52856ed7
sheldonwafula <Sheldon@DESKTOP-U1K27PB>
2023-05-19 12:14:26 -0700

    Add error text

--- validate.js
+++ validate.js
@@ -1,36 +1,62 @@
-const form = document.querySelector('.contact');
-const email = document.getElementById('mail');
-const error = email.nextElementSibling;
+const form = document.querySelector('.form-main');
+const clientname=document.querySelector('#client-name')
+const email = document.querySelector('#mail');
+const clienttext=document.querySelector('#client-message')
+const error = document.querySelector('.error');
+const submi=document.querySelector('#sumission')
 
-const regex = /[a-z]/;
+
+submi.addEventListener('click', (e) => {
+  e.preventDefault();
+  error.innerHTML = '';
+  if (clientname.value === '' || clientname.value == null) {
+    error.innerHTML = 'enter your name';
+  } else if (email.value === '' || email.value == null) {
+    error.innerHTML = ' enter your email';
+  } else if (email.value !== email.value.toLocaleLowerCase()) {
+    error.innerHTML = 'Please use lower case ONLY';
+    email.className = 'invalid';
+    error.className = 'error';
+    error.className = 'error active';
+  } else if (clienttext.value === '' || clienttext.value == null) {
+    clienttext.className = 'invalid';
+    error.innerHTML = 'Please enter your message';
+    error.className = 'error';
+  } else {
+    form.submit();
+  }
+});
+
+
+
 
 // checking if the email field is empty
-window.addEventListener('load', () => {
-  const isValid = email.value.length === 0 || regex.test(email.value);
-  email.className = isValid ? 'valid' : 'invalid';
-});
+// window.addEventListener('load', () => {
+//   const isValid = email.value.length === 0 || regex.test(email.value);
+//   email.className = isValid ? 'valid' : 'invalid';
+// });
 
-email.addEventListener('input', () => {
-  const isValid = email.value.length === 0 || regex.test(email.value);
-  if (isValid) {
-    email.className = 'valid';
-    email.textContent = '';
-    error.className = 'error';
-  } else {
-    email.className = 'invalid';
-  }
-});
+// email.addEventListener('input', () => {
+//   const isValid = email.value.length === 0 || regex.test(email.value);
+//   if (isValid) {
+//     email.className = 'valid';
+//     email.textContent = '';
+//     error.className = 'error';
+//   } else {
+//     email.className = 'invalid';
+//   }
+// });
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const isValid = email.value.length === 0 || regex.test(email.value);
-  if (!isValid) {
-    email.className = 'invalid';
-    error.textContent = 'Invalid email address!';
-    error.className = 'error active';
-  } else {
-    email.className = 'valid';
-    error.textContent = '';
-    error.className = 'error';
-  }
-});
+// form.addEventListener('submit', (e) => {
+//   e.preventDefault();
+//   const isValid = email.value.length === 0 || regex.test(email.value);
+//   if (!isValid) {
+//     email.className = 'invalid';
+//     error.textContent = 'Invalid email address!';
+//     error.className = 'error active';
+//   } else {
+//     email.className = 'valid';
+//     error.textContent = '';
+//     error.className = 'error';
+//   }
+// });
