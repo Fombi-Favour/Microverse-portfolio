88d2a3a3cbd1438126dc674ac20e993c1e1fec24
sheldonwafula <Sheldon@DESKTOP-U1K27PB>
2023-05-19 02:59:01 -0700

    Add year property to the object

--- popup.js
+++ popup.js
@@ -4,8 +4,8 @@ const popup = document.getElementById('pop');
 const Title = document.querySelector('.title');
 const Author = document.querySelector('.author');
 const dot = document.getElementsByClassName('dot');
-// const [spec1] = document.querySelector('.spec1');
-// const [spec2] = document.querySelector('.spec2');
+const spec1 = document.querySelector('.spec1');
+const spec2 = document.querySelector('.spec2');
 const pic = document.querySelector('.project-image');
 const desc = document.querySelector('.description');
 const tag = document.querySelector('.tag');
@@ -17,7 +17,8 @@ const details = [
     title: 'Tonic',
     image: './assets/project1.svg',
     author: 'CANOPY',
-    spec: ['Back End Dev', '2015'],
+    spec: 'Full Stack Dev', 
+    Year:'2015',
     description:
       'A daily selection of privately personalized reads; no accounts or sign-ups required.',
     technologies: ['HTML', 'CSS', 'JavaScript'],
@@ -29,7 +30,8 @@ const details = [
     title: 'Uber Navigation',
     image: './assets/project2.svg',
     author: 'Uber',
-    spec: ['Lead Developer', '2018'],
+    spec: 'Full Stack Dev', 
+    Year:'2015',
     description:
       'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
     technologies: ['HTML', 'Ruby on Rails', 'CSS', 'JavaScript'],
@@ -41,7 +43,8 @@ const details = [
     title: 'Facebook 360',
     image: './assets/project3.svg',
     author: 'FACEBOOK',
-    spec: ['Full Stack Dev', '2015'],
+    spec: 'Full Stack Dev', 
+    Year:'2015',
     description:
       "Exploring the future of media in Facebook's first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.",
     technologies: ['HTML', 'Ruby on Rails', 'CSS', 'JavaScript'],
@@ -53,7 +56,8 @@ const details = [
     title: 'Multi-Post Stories',
     image: './assets/project4.svg',
     author: 'FACEBOOK',
-    spec: ['Full Stack Dev', '2015'],
+    spec: 'Full Stack Dev', 
+    Year:'2015',
     description:
       'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
     technologies: ['HTML', 'Ruby on Rails', 'CSS', 'JavaScript'],
@@ -73,9 +77,9 @@ details.forEach((detail) => {
           <div class="content">
             <h3 class="author">${detail.author}</h3>
             <img src="./assets/Counter.svg" alt="dot" />
-            <h3>${detail.spec[0]}</h3>
+            <h3>${detail.spec}</h3>
             <img src="./assets/Counter.svg" alt="dot" />
-            <h3>${detail.spec[1]}</h3>
+            <h3>${detail.Year}</h3>
           </div>
           <p>
             ${detail.description}
@@ -92,8 +96,8 @@ details.forEach((detail) => {
 function showDetails(Id) {
   Title.textContent = details[Id].title;
   Author.textContent = details[Id].author;
-  // spec1.innerHTML = details[Id].spec;
-  // spec2.innerHTML = details[Id].spec;
+  spec1.innerHTML = details.spec;
+  spec2.innerHTML = details.Year;
   pic.src = details[Id].image;
   desc.textContent = details[Id].description;
   tag.innerHTML = details[Id].technologies.map((tags) => `<li>${tags}</li>`).join('');
